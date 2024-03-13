27a25746fe14c3aab86c52289582d9cf990d6237
newmio <newm.dev@gmail.com>
2024-03-13 17:19:10 +0200

    init 2

---
+++ internal/app/db/postgres/postgres.go
@@ -0,0 +1,48 @@
+package postgres
+
+import (
+	"fmt"
+	"searcher/internal/app/db"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
+)
+
+func OpenDb() (*sqlx.DB, error) {
+	v := viper.New()
+	v.AddConfigPath("internal/app/db/postgres")
+	v.SetConfigName("config")
+	err := v.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := initDb(db.Config{
+		Host:     v.GetString("host"),
+		Port:     v.GetString("port"),
+		User:     v.GetString("user"),
+		Password: v.GetString("password"),
+		DbName:   v.GetString("dbName"),
+		SslMode:  v.GetString("sslMode"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func initDb(c db.Config) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		c.Host, c.Port, c.User, c.DbName, c.Password, c.SslMode))
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
---
+++ internal/app/db/redis/redis.go
@@ -0,0 +1,54 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"searcher/internal/app/db"
+	"strconv"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/spf13/viper"
+)
+
+func OpenDb() (*redis.Client, error) {
+	v := viper.New()
+	v.AddConfigPath("internal/app/db/redis")
+	v.SetConfigName("config")
+	err := v.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := initDb(db.Config{
+		Host:     v.GetString("host"),
+		Port:     v.GetString("port"),
+		User:     v.GetString("user"),
+		Password: v.GetString("password"),
+		DbName:   v.GetString("dbName"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func initDb(c db.Config) (*redis.Client, error) {
+	dbName, err := strconv.Atoi(c.DbName)
+	if err != nil {
+		return nil, err
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", c.Host, c.Port),
+		Password: c.Password,
+		DB:       dbName,
+	})
+
+	_, err = client.Ping(context.Background()).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
